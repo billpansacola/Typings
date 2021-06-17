9e515cf9d304f316e5a04c97b570729794a96738
billpansacola <48074272+billpansacola@users.noreply.github.com>
2021-06-16 17:39:17 -0700

    adjusted so the user can't type into the textbox

--- Game.py
+++ Game.py
@@ -25,7 +25,7 @@ class Game:
     def start(self):
         root = Tk()
         entry = Entry(root)
-        text = Text(root, height=5, padx=10, pady=2, wrap=WORD, font="Courier")
+        text = Text(root, width=50, height=5, padx=5, pady=2, wrap=WORD, font="Courier", bg="#F1F1F1")
         text.tag_config("mistake", foreground="red")
         text.tag_config("correct", foreground="green")
 
@@ -37,12 +37,17 @@ class Game:
             
             userInput = entry.get()
             entry.delete(0, END)
+            if len(userInput) == 1:
+                return
+            
+            text.config(state="normal")
             if self.compare(self.words[self.currWord], userInput[:-1]):
                 text.tag_add("correct", "1." + str(self.currIndex), "1." + str(self.currIndex+len(self.words[self.currWord])))
                 self.correct += 1
             else:
                 text.tag_add("mistake", "1." + str(self.currIndex), "1." + str(self.currIndex+len(self.words[self.currWord])))
-
+            text.config(state="disabled")
+            
             self.currIndex += len(self.words[self.currWord]) + 1
             self.currWord += 1
 
@@ -50,6 +55,7 @@ class Game:
                 endGame()
         
         def redo():
+            text.config(state="normal")
             entry.delete(0,END)
             self.currWord = 0
             self.correct = 0
@@ -58,8 +64,10 @@ class Game:
             self.displayText = Words().convertListToString(self.words)
             text.delete(1.0, "end")
             text.insert(INSERT, self.displayText)
+            text.config(state="disabled")
 
         text.insert(INSERT, self.displayText)
+        text.config(state="disabled")
         redo_button = Button(root, text="Redo", command=redo)
 
         text.grid(row=0, column=0)
